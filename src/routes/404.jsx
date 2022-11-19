be713c65908f29fd6cb5637277d26016ccc68b39
tonmoydeb404 <tonmoydeb404@gmail.com>
2022-11-19 16:14:26 +0600

    home page complete

--- src/routes/404.jsx
+++ src/routes/404.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const NotFound = () => {
-  return <div>Error 404: Page not found</div>;
+  return (
+    <div className="container">
+      <div className="flex py-32 min-h-screen flex-col items-center justify-center">
+        <h2 className="text-4xl font-bold">ERROR 404</h2>
+        <p className="text-sm mt-2">page not found</p>
+
+        <Link to={"/"} className="mt-10 btn btn-primary">
+          Back to Home
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default NotFound;
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -3,11 +3,26 @@ import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div>
-      <h1>This is home page</h1>
-      <Link className="btn btn-primary" to={"/dashboard"}>
-        Dashboard
-      </Link>
+    <div className="container">
+      <div className="flex py-32 min-h-screen flex-col sm:items-center justify-center">
+        <img src="/images/logo.png" alt="" className="w-24" />
+
+        <h2 className="font-bold text-3xl sm:text-4xl mt-6">KORMO Tracker</h2>
+        <p className="mt-2 text-sm tracking-wide">
+          track your daily tasks using kormo tracker app
+        </p>
+
+        <div className="flex flex-wrap items-center gap-2 mt-10">
+          <Link className="btn btn-primary gap-1" to={"/dashboard"}>
+            Get Started
+            <i className="bi bi-arrow-right"></i>
+          </Link>
+          <Link className="btn btn-warning gap-1" to={"#"}>
+            Github
+            <i className="bi bi-github"></i>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
